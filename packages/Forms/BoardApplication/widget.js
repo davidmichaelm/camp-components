d4ebfe34c46499abaac05f74e891c699f4ebf2d2
David Marquardt <davemarquardt@gmail.com>
2022-01-09 19:29:24 -0600

    Update dependencies, add @emotion/styled, add mui theme

--- packages/Forms/BoardApplication/widget.js
+++ packages/Forms/BoardApplication/widget.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {BoardApplication} from './';
+import {createTheme, ThemeProvider} from '@mui/material'
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: "#0c726c"
+        }
+    }
+});
 
 ReactDOM.render(
     <React.StrictMode>
-        <BoardApplication />
+        <ThemeProvider theme={theme}>
+            <BoardApplication/>
+        </ThemeProvider>
     </React.StrictMode>,
     document.getElementById('board-application')
 );
