b1afc7e83de46a5f358057008f4394d5ddfbdd8f
David Marquardt <davemarquardt@gmail.com>
2022-07-01 19:55:31 -0500

    Add calendar button, align border feather

--- packages/Events/components/EventsContainer/index.js
+++ packages/Events/components/EventsContainer/index.js
@@ -33,8 +33,18 @@ const EventContainer = () => {
             <div className={styles.campEventsApp}>
                 <div className={styles.eventsHeader}>
                     <h1>What's going on at Camp?</h1>
+                    <a className={styles.calendarButton} href="/calendar">
+                        Check out our calendar
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+                             stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                             className="feather feather-calendar">
+                            <rect x="3" y="4" width="18" height="18" rx="2" ry="2"></rect>
+                            <line x1="16" y1="2" x2="16" y2="6"></line>
+                            <line x1="8" y1="2" x2="8" y2="6"></line>
+                            <line x1="3" y1="10" x2="21" y2="10"></line>
+                        </svg>
+                    </a>
                 </div>
-
                 <div className={styles.eventsContainer}>
                     {
                         events.map((event, index) => {
