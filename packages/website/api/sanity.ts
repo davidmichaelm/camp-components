3e174ada726faecd7a0ca7c609e7c6272d064710
David Marquardt <davemarquardt@gmail.com>
2023-06-24 20:35:52 -0500

    Order rates on fetch

--- packages/website/api/sanity.ts
+++ packages/website/api/sanity.ts
@@ -11,6 +11,6 @@ interface ApiRateCategory {
 }
 
 export const fetchRates = async () => {
-    const groq = `*[_type == 'rateCategory']`;
+    const groq = `*[_type == 'rateCategory']|order(orderRank)`;
     return sanityClient.fetch<ApiRateCategory[]>(groq);
 };
