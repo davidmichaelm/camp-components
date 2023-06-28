6c651c369e7d113bf4dcfd2bbf503ce8a8df2dd3
David Marquardt <davemarquardt@gmail.com>
2023-06-27 21:39:52 -0500

    Banners: Remove active filter

--- packages/Banners/components/BannerContainer/BannerContainer.js
+++ packages/Banners/components/BannerContainer/BannerContainer.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 import { sanityClient } from "@campphillip/common";
 import { Banner } from "../Banner";
 import * as styles from "./banner-container.module.css";
@@ -8,19 +8,22 @@ export const BannerContainer = () => {
     const [loading, setLoading] = useState(true);
 
     useEffect(() => {
-        const query = "*[_type == 'banner' && active == true]";
-        sanityClient.fetch(query)
-            .then(banners => {
+        const query = "*[_type == 'banner']";
+        sanityClient
+            .fetch(query)
+            .then((banners) => {
                 setBanners(banners);
             })
-            .catch(error => console.log(error))
+            .catch((error) => console.log(error))
             .finally(() => setLoading(false));
     }, []);
 
     return (
         <div className={styles.bannerContainer}>
             <div className={styles.bannerApp}>
-                {banners.map(banner => <Banner {...banner} loading={loading} />)}
+                {banners.map((banner) => (
+                    <Banner {...banner} loading={loading} />
+                ))}
             </div>
         </div>
     );
