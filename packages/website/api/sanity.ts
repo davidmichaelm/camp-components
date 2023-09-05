e49672e64d5ad7a8f1313257e0189e2b299a0c53
David Marquardt <davemarquardt@gmail.com>
2023-09-04 21:19:31 -0500

    Add category fetching stub

--- packages/website/api/sanity.ts
+++ packages/website/api/sanity.ts
@@ -14,3 +14,8 @@ export const fetchRates = async () => {
     const groq = `*[_type == 'rateCategory']|order(orderRank)`;
     return sanityClient.fetch<ApiRateCategory[]>(groq);
 };
+
+export const fetchCategories = async () => {
+    // return fetch("/categories");
+    return [];
+};
