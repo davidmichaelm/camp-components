5b636106d2c894e994e8119817715a6a402b3220
David Marquardt <davemarquardt@gmail.com>
2023-12-22 15:11:57 -0600

    Add image type to sanity

--- packages/sanity/schemas/events.ts
+++ packages/sanity/schemas/events.ts
@@ -14,6 +14,18 @@ const eventFields = [
         validation: (Rule) => Rule.required(),
     }),
     defineField({
+        title: "Image Type",
+        name: "imageType",
+        type: "string",
+        options: {
+            list: [
+                { title: "Default", value: "default" },
+                { title: "Square", value: "square" },
+            ],
+            layout: "radio",
+        },
+    }),
+    defineField({
         title: "Start Date",
         name: "startDate",
         type: "datetime",
@@ -47,6 +59,9 @@ export const events = defineType({
     name: "event",
     type: "document",
     fields: eventFields,
+    initialValue: {
+        imageType: "default",
+    },
 });
 
 export const boardEvents = defineType({
